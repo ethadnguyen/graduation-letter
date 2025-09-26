e1d9d0ef8d6aef55e91e611c2c4bce32cb314b0d
ethadnguyen <tringuyen1382003@gmail.com>
2025-09-26 20:43:35 +0700

    delete tailwind config

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  darkMode: ["class"],
-  content: ["./src/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
-  theme: { extend: {} },
-  plugins: [require("tailwindcss-animate")],
-};
-export default config;
